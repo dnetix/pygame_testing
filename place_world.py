8a8854977a73c902574c179f88c6157e4433411d
Diego Calle <dnetix@gmail.com>
2018-08-24 17:03:19 -0500

    Fix code for this example according to library changes

--- place_world.py
+++ place_world.py
@@ -6,8 +6,6 @@ pygame.init()
 WIDTH = 500
 HEIGHT = 500
 
-worldImage = pygame.image.load('./assets/world_70.png')
-
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Testing game")
 
@@ -15,23 +13,22 @@ clock = pygame.time.Clock()
 
 ended = False
 
-x = mylibpg.StaticAsset('./assets/world_70.png', (100, 100))
-x.visualize()
+w = mylibpg.StaticAsset(screen, './assets/world_70.png', (100, 100))
 
 while not ended:
     screen.fill(mylibpg.COLOR_BLACK)
 
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
-            x.placeMiddle(event.pos)
+            # Allows to place the asset where the mouse clicks
+            w.place_middle(event.pos)
             # visualize(event.pos[0], event.pos[1])
 
         if event.type == pygame.QUIT:
             ended = True
 
-    x.visualize()
+    w.visualize()
     pygame.display.update()
-    # pygame.display.flip()
 
     clock.tick(24)
 
